aea4542aafe1f045f22f83e8347fd1dd82209e13
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-01-11 01:08:42 -0600

    Sytnax Practice
    Basic Pracitce with Commenting, Indenting Sentences, New Lines, and more.

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
+world = 53
+print(world)
 
+#back slash n creates a new line and back slash t adds a indent
+print('hello worlds!  \nToday is going great!')
+
+#using a end with a space in between quotations gives a space at the end of the one string
+print('one', end=' ')
+print('two')
+
+
+print('One,\'two\',\"three\",four, \\five\\\n\tonce,\'I\' caught a\ fish\n\t\t\'//alive\\\\\'')
