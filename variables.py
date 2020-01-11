843e4c66e19a2d1079b6ef82f91a09a054c11d2b
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-01-11 02:23:03 -0600

    Left Off on Function Calls
    Function calls

---
+++ variables.py
@@ -0,0 +1,31 @@
+"""
+Created on Sat Jan 11 01:11:35 2020
+
+@author: oscarpacheco
+"""
+
+# storing information into a variable 
+
+
+#this is assignment statment, variable with a value
+x = 9
+
+y = 12
+
+z = x + y
+
+print(z)
+
+#no spaces are allowed in variable names
+
+oscarsWorld = 'hello world'
+
+print(oscarsWorld)
+
+
+#An expression is a combination of values, variables, and operators
+
+first = 'ice'
+second = 'cream'
+
+print(first + second)
