75899983315653774e017aec9a1706c072acd93a
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-01-19 03:28:35 -0600

    Question 1
    Learning how to add strings and print out a user's input

---
+++ challenges.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 03:06:38 2020
+
+@author: oscarpacheco
+"""
+
+print('Challenges File Invoked \n ')
+
+#ask the user for the radius of a circle and then print the area
+
+radius = float(input('Please enter the circle radius:' ))
+pi = 3.14159
+area = pi * radius**2
+print('You entered', radius, 'the area of the circle is', area)
