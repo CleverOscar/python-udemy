5ecb48d93d8e362c9ae6ee354ff9b8b9760c0de0
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-01-19 03:04:16 -0600

    Python Input Method
    Printing Strings, Integers, and more!
    Writing a program next

---
+++ exercise/printing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 02:21:59 2020
+
+@author: oscarpacheco
+"""
+
+user_input = int(input('How many apples do you have \n '));
