81ddae5342c97ac42e8717e26b4898797fcc6fa2
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-01-19 03:45:42 -0600

    Tempeture Conversion
    Converting Faharenhiet to Celcius

--- challenges.py
+++ challenges.py
@@ -10,7 +10,13 @@ print('Challenges File Invoked \n ')
 
 #ask the user for the radius of a circle and then print the area
 
-radius = float(input('Please enter the circle radius:' ))
-pi = 3.14159
-area = pi * radius**2
-print('You entered', radius, 'the area of the circle is', area)
+#radius = float(input('Please enter the circle radius:' ))
+#pi = 3.14159
+#area = pi * radius**2
+#print('You entered', radius, 'the area of the circle is', area)
+
+#Convert fahrenheit to celcius
+
+fah = float(input('Enter tempature here:'))
+cel = (fah - 32) * 5/9
+print(fah,'Fahrenheit in Celsius is:2',cel)
