a1c4fd14031a0d3355b2787c93a32ac20d2c616a
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-01-20 17:47:53 -0600

    Create simple_repetition.py

---
+++ simple_repetition.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#loop repetition 
+
+#for i in range(4):
+#    print('hello')
+
+#for x in range(0,3):
+#    print("we're on time %d" % (x))
+
+#name = 'Oscar'
+#
+#print("hello, %s!" % name)
+
+
+#name = 'oscar'
+#age = 23
+#
+#print("%s is %d years old." % (name,age))
+
+#mylist = [1,2,3]
+#
+#print("A list of numbers %s" % mylist)
+
+#exercise
+
+data = ('John', 'Doe', 53.55)
+
+format_string = "Hello %s $s. \n Your current balance is $%s."
+
+print(format_string % data)
