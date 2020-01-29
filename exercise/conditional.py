48b8002693b6457d53a03e90e866a6c59b230940
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-01-29 10:15:58 -0600

    Update conditional.py

--- exercise/conditional.py
+++ exercise/conditional.py
@@ -1,39 +1,43 @@
-# -*- coding: utf-8 -*-
-x = 5
-y = 6
+## -*- coding: utf-8 -*-
+#x = 5
+#y = 6
+#
+#print('x =',x,'y =', y)
+#print('X is less than Y:', x<y)
+#print('X is greater than Y:',x>y)
+#
+#var_1 = 7
+#var_2 = 7
+#
+#print('Var_1:', var_1, 'Var_2:', var_2)
+#print(var_1 < var_2)
+#print(var_1 > var_2)
+#print(var_1 == var_2)
+#print(var_1 <= var_2)
+#print(var_1 >= var_2)
+#print(var_1 != var_2)
+#
+#
+##if statement practice 
+#
+#some_condition = True
+#
+#if some_condition:
+#    print('The variable some_condition is TRUE')
+#else: 
+#    print('The variable some_condition is False')
+#    
+#    
+#temp = int(input('Please enter the temperatue in Celsius. An integer between 0-40:>>>'))
+#
+#if temp > 30: 
+#    print('It is warm outside!')
+#elif temp <= 30 and temp > 20:
+#    print('It is warm but no time to wear shorts!')
+#elif temp <= 20 and temp > 10: 
+#    print('You will probably need a coat!')
+#else: print('TOO COLD OUT!')
 
-print('x =',x,'y =', y)
-print('X is less than Y:', x<y)
-print('X is greater than Y:',x>y)
 
-var_1 = 7
-var_2 = 7
+my_string = 'Python'
 
-print('Var_1:', var_1, 'Var_2:', var_2)
-print(var_1 < var_2)
-print(var_1 > var_2)
-print(var_1 == var_2)
-print(var_1 <= var_2)
-print(var_1 >= var_2)
-print(var_1 != var_2)
-
-
-#if statement practice 
-
-some_condition = True
-
-if some_condition:
-    print('The variable some_condition is TRUE')
-else: 
-    print('The variable some_condition is False')
-    
-    
-temp = int(input('Please enter the temperatue in Celsius. An integer between 0-40:>>>'))
-
-if temp > 30: 
-    print('It is warm outside!')
-elif temp <= 30 and temp > 20:
-    print('It is warm but no time to wear shorts!')
-elif temp <= 20 and temp > 10: 
-    print('You will probably need a coat!')
-else: print('TOO COLD OUT!')
