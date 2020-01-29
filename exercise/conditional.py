b148201292e5ddbc539207e0c7865a73ad47c744
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-01-29 08:50:15 -0600

    Update conditional.py
    Python Notes: More operators to work with

--- exercise/conditional.py
+++ exercise/conditional.py
@@ -5,3 +5,14 @@ y = 6
 print('x =',x,'y =', y)
 print('X is less than Y:', x<y)
 print('X is greater than Y:',x>y)
+
+var_1 = 7
+var_2 = 7
+
+print('Var_1:', var_1, 'Var_2:', var_2)
+print(var_1 < var_2)
+print(var_1 > var_2)
+print(var_1 == var_2)
+print(var_1 <= var_2)
+print(var_1 >= var_2)
+print(var_1 != var_2)
